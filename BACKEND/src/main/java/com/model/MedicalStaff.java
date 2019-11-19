70102962b0271fef85fe3f77781623640765aa1c
nemanjavasic97 <57145230+nemanjavasic97@users.noreply.github.com>
2019-11-19 14:18:53 +0100

    model change

--- BACKEND/src/main/java/com/model/MedicalStaff.java
+++ BACKEND/src/main/java/com/model/MedicalStaff.java
@@ -14,7 +14,7 @@ public class MedicalStaff extends User{
         this.clinic = clinic;
     }
 
-    public MedicalStaff(String firstName, String secondName, String password, String email, String address, String city, String state, String telephone, String uniqueNum, String id, Clinic clinic) {
+    public MedicalStaff(String firstName, String secondName, String password, String email, String address, String city, String state, String telephone, String uniqueNum, Long id, Clinic clinic) {
         super(firstName, secondName, password, email, address, city, state, telephone, uniqueNum, id);
         this.clinic = clinic;
     }
--- BACKEND/src/main/java/com/model/Nurse.java
+++ BACKEND/src/main/java/com/model/Nurse.java
@@ -7,7 +7,7 @@ public class Nurse extends MedicalStaff{
 
 
 
-    public Nurse(String firstName, String secondName, String password, String email, String address, String city, String state, String telephone, String uniqueNum, String id, Clinic clinic) {
+    public Nurse(String firstName, String secondName, String password, String email, String address, String city, String state, String telephone, String uniqueNum, Long id, Clinic clinic) {
         super(firstName, secondName, password, email, address, city, state, telephone, uniqueNum, id, clinic);
     }
 }
