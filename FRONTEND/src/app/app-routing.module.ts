816fefacb7d2c6fa3cb8bd92508faf30f83ab612
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2019-12-05 17:58:02 +0100

    add routes

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import {RegisterPatientComponent} from './components/register-patient/register-p
 import {PatientPageComponent} from './components/patient-page/patient-page.component'
 import { DoctorPageComponent } from './components/doctor-page/doctor-page.component';
 import {PatientProfileComponent} from './components/patient-profile/patient-profile.component'
+import { MedicalRecordPageComponent } from './components/medical-record-page/medical-record-page.component';
+import { MedicalHistoryPageComponent } from './components/medical-history-page/medical-history-page.component';
+import { ClinicsPageComponent } from './components/clinics-page/clinics-page.component';
+import { DoctorProfileComponent } from './components/doctor-profile/doctor-profile.component';
 
 const routes: Routes = [
   {
@@ -28,8 +32,21 @@ const routes: Routes = [
     component: DoctorPageComponent
   },
   {
+    path:'doctor/profile',
+    component: DoctorProfileComponent
+  },
+  {
     path:'patient/profile',
     component: PatientProfileComponent
+  },{
+    path:'patient/medicalRecord',
+    component: MedicalRecordPageComponent
+  },{
+    path:'patient/medicalHistory',
+    component: MedicalHistoryPageComponent
+  },{
+    path:'patient/clinics',
+    component: ClinicsPageComponent
   }
 ];
 
