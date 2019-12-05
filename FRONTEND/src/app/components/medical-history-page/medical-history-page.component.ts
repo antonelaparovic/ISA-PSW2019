cdc5f05b5cda33173ea84e17871ad7e01d1442bd
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2019-12-05 17:56:30 +0100

    add history page component

---
+++ FRONTEND/src/app/components/medical-history-page/medical-history-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-history-page',
+  templateUrl: './medical-history-page.component.html',
+  styleUrls: ['./medical-history-page.component.css']
+})
+export class MedicalHistoryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
