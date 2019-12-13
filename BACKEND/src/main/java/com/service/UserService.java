4bbf736ffd312cbb9b1af0cb2675a8804e1bb870
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2019-12-13 13:47:08 +0100

    create user service

--- BACKEND/src/main/java/com/service/UserService.java
+++ BACKEND/src/main/java/com/service/UserService.java
@@ -5,6 +5,8 @@ import com.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -16,5 +18,40 @@ public class UserService {
     }
     public User save(User user){
         return userRepo.save(user);
+    }
+    public List<User> findall()
+    {
+        return userRepo.findAll();
+    }
+
+    public User getUser(String email){
+        List<User> tmp = findall();
+        if(tmp.size() == 0)
+            return null;
+
+        for(User u : tmp)
+        {
+            if(u.getEmail().equals(email))
+                return u;
+        }
+
+        return null;
+    }
+
+    public boolean editUser(User u) {
+        List<User> tmp = findall();
+        if(tmp.size() == 0)
+            return false;
+
+        for(User u1 : tmp)
+        {
+            if(u.getEmail().equals(u1.getEmail())) {
+                u1 = u;
+                userRepo.save(u1);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
