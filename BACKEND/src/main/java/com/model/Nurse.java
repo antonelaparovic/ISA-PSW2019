3af18cad9ad6f95aa73619fac254618faf73500d
antonelaparovic <57138968+antonelaparovic@users.noreply.github.com>
2019-12-13 17:00:58 +0100

    Update Nurse model

--- BACKEND/src/main/java/com/model/Nurse.java
+++ BACKEND/src/main/java/com/model/Nurse.java
@@ -12,11 +12,30 @@ public class Nurse {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String surname;
+
+    @Column(columnDefinition = "VARCHAR(11)", unique = true, nullable = false)
+    private String number;
+
+
     @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String country;
 
     @Column(columnDefinition = "VARCHAR(30)", nullable = false)
     private String firstName;
@@ -43,4 +62,91 @@ public class Nurse {
     private Set<Prescription> prescriptions = new HashSet<>();
 
 
+    public  Nurse() {};
+
+    public Nurse(Long id, String email, String password, String name, String surname, String number, String city, String address, String country){
+        this.id=id;
+        this.email=email;
+        this.password=password;
+        this.name=name;
+        this.surname=surname;
+        this.number=number;
+        this.address=address;
+        this.city=city;
+        this.country=country;
+        }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
+
+
