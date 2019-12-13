a459ad0799d9cc05a97a6c7f3b29201ddcf22638
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2019-12-13 13:46:07 +0100

    add clinic model

---
+++ FRONTEND/src/app/models/clinic.ts
@@ -0,0 +1,14 @@
+
+export class Clinic {
+    id: number;
+    name: string;
+    address: string;
+    description: string;
+  
+    constructor(name: string, address: string, description: string, id?: number) {
+      this.name = name;
+      this.address = address;
+      this.description = description;
+      this.id = id;
+    }
+  }
