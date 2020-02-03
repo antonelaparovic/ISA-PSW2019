3e722ebbb841c448cda851202219fc64faf3ae47
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-02-03 22:18:01 +0100

    update medical record component

--- FRONTEND/src/app/components/medical-record-page/medical-record-page.component.ts
+++ FRONTEND/src/app/components/medical-record-page/medical-record-page.component.ts
@@ -1,24 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MedicalRecord } from 'src/app/models/medicalRecord';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
 import { MedicalRecordService } from 'src/app/services/medical-record.service';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
+import { PatientProfileComponent } from '../patient-profile/patient-profile.component';
+import { Patient } from 'src/app/models/patient';
+import { MedicalRecord } from 'src/app/models/medicalRecord';
 
-export class Table{
-  height:number;
-    weight:number;
-    bloodType:string;
-    allergies:string;
-
-    constructor(height:number,weight:number,bloodType:string,allergies:string){
-        this.height=height;
-        this.weight=weight;
-        this.bloodType=bloodType;
-        this.allergies=allergies;
-    }
-
-}
 
 @Component({
   selector: 'app-medical-record-page',
@@ -26,21 +14,24 @@ export class Table{
   styleUrls: ['./medical-record-page.component.css']
 })
 export class MedicalRecordPageComponent implements OnInit {
-
-
-  displayedColumns: string[] = ['Height', 'Weight','BloodType','Allergies'];
+  
+  
+  records: Array<MedicalRecord> = new Array<MedicalRecord>();
+  loggedUser: string = this.userService.isLoggedIn();
+  user: User;
+  mr: MedicalRecord;
+  displayedColumns: string[] = ['Height', 'Weight','BloodType','Allergies','ExaminationReports'];
   dataSource= new MatTableDataSource<MedicalRecord>();
-  record:MedicalRecord;
-  records:Array<MedicalRecord>=new Array<MedicalRecord>();
-  listTable:Array<Table>=new Array<Table>();
-  user:User;
+  rec:Array<MedicalRecord>=new Array<MedicalRecord>();
+
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   constructor(
-    private medicalRecordService:MedicalRecordService,
-    private userService:UserService
-  ) { 
-    this.records=this.medicalRecordService.getAllRecords();
-    this.user=JSON.parse(userService.isLoggedIn());
+    private medicalRecordService: MedicalRecordService, 
+    private userService: UserService
+    ) {
+      this.records = medicalRecordService.getAllMedicalRecords();
+      this.user = JSON.parse(this.loggedUser);
+      this.all();
   }
 
   ngOnInit() {
@@ -48,19 +39,25 @@ export class MedicalRecordPageComponent implements OnInit {
   }
 
   all() {
-    this.fillData();
-    this.dataSource = new MatTableDataSource(this.listTable);
+    this.setAll();
+    this.dataSource = new MatTableDataSource(this.rec);
+    this.dataSource.paginator = this.paginator;
 }
 
-fillData(){
-  /*for (let i = 0; i < this.records.length; i++) {
-    const element = this.records[i];
-          const table=new Table(element.height,element.weight,element.bloodType,element.allergies);
-          this.listTable.push(table);
-          
-  }*/
-  const table=new Table(180,85,"A+","No allergies");
-  this.listTable.push(table);
-}
+  setAll() {
+    for (let i = 0; i < this.records.length; i++) {
+      const element = this.records[i];
+      let truee=0;
+      if(element.patient.email==this.user.email){
+        for(const c of this.rec){
+          if(c.id==element.id)
+          truee=1;
+        }
+        if(truee==0){
+          this.rec.push(this.records[i]);
+        }
+      }
+  }
+  }
 
 }
