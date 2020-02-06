c0cde5935acf36ec6c9555e8dfd564be1cd2b6b7
nemanjavasic97 <57145230+nemanjavasic97@users.noreply.github.com>
2020-02-07 00:48:06 +0100

    Create clinicadministratorprofile komponente

---
+++ FRONTEND/src/app/components/clinicadministrator-profile/clinicadministrator-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clinicadministrator-profile',
+  templateUrl: './clinicadministrator-profile.component.html',
+  styleUrls: ['./clinicadministrator-profile.component.css']
+})
+export class ClinicadministratorProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
