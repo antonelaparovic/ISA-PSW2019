f81fe2c47192341c843c1669f7e6f9c371d96df4
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-02-06 14:30:53 +0100

    change model

--- FRONTEND/src/app/models/clinic.ts
+++ FRONTEND/src/app/models/clinic.ts
@@ -1,14 +1,22 @@
+import { Doctor } from './doctor';
+import { ExaminationType } from './examinationType';
 
 export class Clinic {
-    id: number;
-    name: string;
-    address: string;
-    description: string;
-  
-    constructor(name: string, address: string, description: string, id?: number) {
-      this.name = name;
-      this.address = address;
-      this.description = description;
-      this.id = id;
-    }
+  id: number;
+  name: string;
+  address: string;
+  description: string;
+  clinicRating: number;
+  doctors: Array<Doctor> = new Array<Doctor>();
+  examinationTypes: Array<ExaminationType> = new Array<ExaminationType>();
+
+  constructor(name: string, address: string, description: string, doctors: Array<Doctor>, examinationTypes?: Array<ExaminationType>, clinicRating?: number, id?: number) {
+    this.name = name;
+    this.address = address;
+    this.description = description;
+    this.doctors = doctors;
+    this.clinicRating = clinicRating;
+    this.examinationTypes = examinationTypes;
+    this.id = id;
   }
+}
