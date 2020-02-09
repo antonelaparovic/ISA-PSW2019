a40046307d7ba834a440a0560608cc1a545e211b
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-02-09 06:04:47 +0100

    update constuctor

--- FRONTEND/src/app/models/examinationTable.ts
+++ FRONTEND/src/app/models/examinationTable.ts
@@ -2,10 +2,14 @@ export class ExaminationTable{
      kind: string;
    doctors:string;
    clinic: string;
+   label:string;
+   price:string;
 
-    constructor(kind: string,doctors:string,clinic:string) {
+    constructor(kind: string,doctors:string,clinic:string,label:string,price:string) {
       this.kind = kind;
       this.doctors=doctors;
       this.clinic=clinic;
+      this.label=label;
+      this.price=price;
   }
 }
