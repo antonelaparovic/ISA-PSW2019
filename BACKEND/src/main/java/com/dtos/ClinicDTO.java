12dd775832bf4203236bbd6137c8683eb14201f5
nemanjavasic97 <57145230+nemanjavasic97@users.noreply.github.com>
2020-02-09 13:16:58 +0100

    Update ClinicDTO

--- BACKEND/src/main/java/com/dtos/ClinicDTO.java
+++ BACKEND/src/main/java/com/dtos/ClinicDTO.java
@@ -1,12 +1,17 @@
 package com.dtos;
 
+import com.model.Doctor;
+import com.model.Room;
+
+import java.util.Set;
+
 public class ClinicDTO {
 
         private String name;
         private String address;
         private String description;
-        private double clinicRating;
-
+        private Set<Doctor> doctors;
+        private Set<Room> rooms;
 
         public String getName() {
             return name;
@@ -28,15 +33,21 @@ public class ClinicDTO {
             return description;
         }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
+        public void setDescription(String description) { this.description = description;};
 
-    public double getClinicRating() {
-        return clinicRating;
+    public Set<Doctor> getDoctors() {
+        return doctors;
     }
 
-    public void setClinicRating(double clinicRating) {
-        this.clinicRating = clinicRating;
+    public void setDoctors(Set<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public Set<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(Set<Room> rooms) {
+        this.rooms = rooms;
     }
 }
