3b6228e101eabd4b0e9c2654dfaed7f82531ffbd
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-02-09 06:14:26 +0100

    get clinic with specific type

--- BACKEND/src/main/java/com/controller/ClinicController.java
+++ BACKEND/src/main/java/com/controller/ClinicController.java
@@ -35,4 +35,46 @@ public class ClinicController{
         }
         return new ResponseEntity<>(clinicService.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/clinic/allWithTypes")
+    public ResponseEntity<List<Clinic>> allWithType(@RequestParam(value = "type", required = true) String type,
+                                                    @RequestParam(value = "date", required = true) String date) {
+
+        List<Clinic> tmp = new ArrayList<>();
+
+        if(type.equals("") || date.equals("")) {
+            return new ResponseEntity<>(tmp, HttpStatus.BAD_REQUEST);
+        }
+
+        List<ExaminationType> types = examinationTypeService.findAll();
+        for (ExaminationType t : types) {
+
+            if(t.getLabel().equals(type)) {
+                tmp.add(t.getClinic());
+            }
+
+        }
+
+        String[] parts = date.split("/");
+        int year = Integer.parseInt(parts[2]);
+        int day = Integer.parseInt(parts[1]);
+        int month = Integer.parseInt(parts[0]);
+        LocalDateTime datetime = LocalDateTime.of(year,month,day,0,0);
+        System.out.println(datetime);
+
+        for (Clinic c : tmp) {
+
+            for(Doctor d : c.getDoctors()) {
+                if(d.getSpecialized().getLabel().equals(type)) {
+                    System.out.println(c.getDoctors());
+                }
+                else
+                    c.getDoctors().remove(d);
+            }
+        }
+
+        System.out.println(tmp.size());
+
+        return new ResponseEntity<>(tmp, HttpStatus.OK);
+    }
 }
