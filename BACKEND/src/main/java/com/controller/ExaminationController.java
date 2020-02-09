c156ffcac373b99f736f2d13fd3f9c64efc57955
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-02-09 05:41:00 +0100

    imports

--- BACKEND/src/main/java/com/controller/ExaminationController.java
+++ BACKEND/src/main/java/com/controller/ExaminationController.java
@@ -2,20 +2,39 @@ package com.controller;
 
 import com.model.Examination;
 import com.service.ExaminationService;
+import com.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 @RestController
 @CrossOrigin(origins = {"http://localhost:4200"})
 public class ExaminationController {
 
         @Autowired
         ExaminationService examinationService;
+
+        @Autowired
+        PatientService patientService;
+
+        @Autowired
+        DoctorService doctorService;
+
+        @Autowired
+        ClinicService clinicService;
+
+        @Autowired
+    ExaminationTypeService examinationTypeService;
 
         @GetMapping(value = "examination/all")
         public ResponseEntity<List<Examination>> all() {
