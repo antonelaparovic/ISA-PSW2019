d5cf774f49650106c02ba13fe2fc159996d85910
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-02-09 05:42:02 +0100

    imports

--- BACKEND/src/main/java/com/service/ExaminationService.java
+++ BACKEND/src/main/java/com/service/ExaminationService.java
@@ -1,6 +1,8 @@
 package com.service;
 
 import com.model.Examination;
+import com.model.ExaminationStatus;
+import com.model.Patient;
 import com.repository.ExaminationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ public class ExaminationService {
 
     @Autowired
     ExaminationRepo examinationRepo;
+    @Autowired
+    MailService mailService;
 
     public List<Examination> findAll() {
         return  examinationRepo.findAll();
